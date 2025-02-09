8523e0f17cb209836ab4e7b7ed56720db3a4c914
ebcwind <153788226+ebcwind@users.noreply.github.com>
2025-02-09 20:05:06 +0800

    Update MFA.py

--- dat/MFA.py
+++ dat/MFA.py
@@ -68,8 +68,6 @@ class MFA(nn.Module):
         )
 
     def forward(self, samples: NestedTensor, net):
-        if not isinstance(samples, NestedTensor):
-            samples = nested_tensor_from_tensor_list(samples)
         features, pos = self.backbone(samples)
 
         srcs = []
